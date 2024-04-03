4edac907531f2a5b5afa17b531b0665a9e1b0cae
Wasim <wasimbhai6205@gmail.com>
2024-04-03 10:50:39 +0530

    create enviornment variable

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ const Freebook = () => {
   useEffect(() => {
     const getBook = async()=>{
       try {
-        const res = await axios.get("http://localhost:4001/book")
+        const res = await axios.get(`http://localhost:4001/book`)
         const data = res.data.filter((data) => data.category === "Free")
         setBook(data)
         console.log(data);
